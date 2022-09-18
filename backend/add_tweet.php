6f89ffce6bb51f01c5db226d1f41ae4376b988e9
HawraaYounes <hawraayounes0@gmail.com>
2022-09-18 17:34:10 +0300

    Adding image with bug

--- backend/add_tweet.php
+++ backend/add_tweet.php
@@ -7,26 +7,26 @@ include("connection/connection.php");
 
 $text = $_POST["text"];
 $user_id=$_POST["user_id"];
+$image_base64=$_POST["base64Image"];
 
 $query = $mysqli->prepare("INSERT INTO tweets(text,user_id) VALUE (?, ?)");
 $query->bind_param("si", $text, $user_id);
 $query->execute();
+$tweetid=1;
+    $file_path="../frontend/images";
+    $image_parts = explode(";base64,", $image_base64);
+    $image_type_aux = explode("image/", $image_parts[0]);
+    $image_type = $image_type_aux[1];
+    $image_base64 = base64_decode($image_parts[1]);
+    $file =  $file_path. uniqid() .$image_type ;
+    echo $file;
+    //file_put_contents($file, $image_base64);
+    move_uploaded_file($file, $image_base64);
 
-// $tweetidQuery=$mysqli->prepare("SELECT LAST_INSERT_ID()");
-// $tweetid=$tweetidQuery->execute();
-// echo $tweetid;
+    $query = $mysqli->prepare("INSERT INTO tweet_images(path,tweet_id,user_id) VALUE (?, ?, ?)");
+    $query->bind_param("sii", $file,$tweetid, $user_id);
+    $query->execute();
 
-// if (isset($_POST['imgUpload'])) {
-//     $base64=$_POST["base64"];
-//     $bin = base64_decode($base64);
-//     $im = imageCreateFromString($bin);
-//     $img_file = './assets';
-//     // $file_path="./assets/".uniqid();
-//     // file_put_contents($file_path,base64_decode($base64));
-//     $imageQuery=$mysqli->prepare("INSERT INTO images(path,tweet_id,user_id) VALUE (?, ?, ?)");
-//     $imageQuery->bind_param("ssi", $file_path, $tweetid,$user_id);
-//     $query->execute();
-// }
 $response = [];
 $response["success"] = true;
 
--- frontend/js/feed.js
+++ frontend/js/feed.js
@@ -1,13 +1,14 @@
 const addTweetBtn=document.getElementById("addTweet-btn");
 const tweetText=document.getElementById("tweet-text");
-const file=document.getElementById("file-input");
-let base64String = "";
-const addTweet=()=>{
+const fileInput=document.getElementById("file-input");
+let base64String;
+const addTweet=(e)=>{
+    e.preventDefault();
     let formData = new FormData();
     formData.append("text", tweetText.value);
     formData.append("user_id", localStorage.getItem('user_id'));
-    formData.append("base64Image", encodeImage(file));
-    formData.append("file", file);
+    formData.append("base64Image", base64String);
+    // formData.append("file", file);
     fetch(`http://localhost/twitter-clone/backend/add_tweet.php `, {
         method: 'POST',
         body: formData
@@ -23,18 +24,18 @@ const addTweet=()=>{
 }
 
 
-function encodeImage(element) {
-    let file = element.files[0];
+function encodeImage(e) {
+    e.preventDefault();
+    let file = this.files[0];
     let reader = new FileReader();
     reader.onloadend = function() {
-      return reader.result;
+       base64String= reader.result;
     }
     reader.readAsDataURL(file);
-    const files = e.target.files;
-    console.log(files)
+
   }
 
 addTweetBtn.addEventListener("click",addTweet)
-
+fileInput.addEventListener("change",encodeImage)
 
 
